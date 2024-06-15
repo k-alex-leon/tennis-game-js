f7e0bbc8454eb3e9e6634b51a0b4decf221c5296
k-alex-leon <kelemon824@gmail.com>
2024-06-14 20:34:11 -0500

    feat: collision finished

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ import "./style.css";
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-
 // GAME STATUS
-let IS_GAME_OVER = true
+let IS_GAME_OVER = true;
+let score = 0;
+let lives = 3;
 // circle coords
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -45,31 +46,81 @@ const brickPadding = 10;
 const brickOffsetTop = 30;
 const brickOffsetLeft = 30;
 // 2d arr with bricks position
-const bricks = new Array(brickColumnCount).fill(
-  new Array(brickRowCount).fill({ x: 0, y: 0 })
-);
-
-// console.log('2d bricks: ', bricks)
+const bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (var r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
 
 // draw bricks on map
 function drawBricks() {
   bricks.map((columns, c) => {
+    // console.log("c: ", c);
     columns.map((brick, r) => {
-      // calculate brick position
-      const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-      const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      console.log("rows: ", brick);
+      if (brick.status) {
+        // console.log("r: ", r);
+        // calculate brick position
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
 
-      brick.x = brickX;
-      brick.y = brickY;
-      // console.log("rows: ", brick);
+        brick.x = brickX;
+        brick.y = brickY;
 
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+
+        console.log("brick: ", brick);
+      }
     });
   });
+
+  // console.log('all bricks: ', bricks)
+}
+
+// collision
+function collisionDetection() {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          if (score === brickRowCount * brickColumnCount) {
+            alert("YOU WIN, CONGRATULATIONS!");
+            document.location.reload()
+          }
+        }
+      }
+    }
+  }
+  // console.log('all bricks:', bricks)
+}
+
+// user score
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText(`Score: ${score}`, 8, 20);
+}
+
+// user lives
+function drawLives() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText(`Lives: ${lives}`, canvas.width - 60, 20);
 }
 
 // user controls
@@ -86,14 +137,22 @@ function keyUpHandler(e) {
   else if (e.key === "Left" || e.key === "ArrowLeft") leftPressed = false;
 }
 
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width)
+    paddleX = relativeX - paddleWidth / 2;
+}
+
 // frames
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // -> clean the canvas
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
+  drawScore();
+  drawLives();
   // hits top || bottom
-  // if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) dy = -dy;
   if (y + dy < ballRadius) {
     dy = -dy;
   } else if (y + dy > canvas.height - ballRadius) {
@@ -101,11 +160,17 @@ function draw() {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     } else {
-      // alert("GAME OVER");
-      console.log('GAME OVER')
-      IS_GAME_OVER = true
-      // document.location.reload();
-      clearInterval(interval);
+      lives--;
+      if (!lives) {
+        alert("GAME OVER");
+        document.location.reload();
+      } else {
+        x = canvas.width / 2;
+        y = canvas.height - 30;
+        dx = 2;
+        dy = -2;
+        paddleX = (canvas.width - paddleWidth) / 2;
+      }
     }
   }
   // hits sides
@@ -117,18 +182,19 @@ function draw() {
   // paddle movement
   if (rightPressed) paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
   else if (leftPressed) paddleX = Math.max(paddleX - 7, 0);
+
+
+  requestAnimationFrame(draw)
 }
-// drawing every 10ms
-// function startGame() {
-//   const interval = setInterval(draw, 10);
-// }
-let interval = null;
 
 // key pressed
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+// mouse listener
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
 document.getElementById("runButton").addEventListener("click", function () {
-  interval = setInterval(draw, 10);
-  // this.disabled = true;
+  draw()
+  this.disabled = true;
 });
